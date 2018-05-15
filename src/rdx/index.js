79bf62b43039a1c639666f6b3c3e3d495c766a4f
BlueAccords <myoldemailname@gmail.com>
2018-05-14 21:16:37 -0700

    initial commit

--- src/rdx/index.js
+++ src/rdx/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers/index";
 
 const store = createStore(rootReducer);
